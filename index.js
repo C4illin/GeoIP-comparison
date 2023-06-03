84937bb53e40c32dc96d16f14b90bb137439ffe3
C4illin <emrik.ostling@gmail.com>
2023-06-03 02:38:54 +0200

    start on url timer

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 const axios = require('axios');
 const sites = require('./sites.json');
+const fs = require('fs');
 
-async function testAPI(api, url) {
+async function testAPI(url) {
   const start = Date.now();
   try {
-    const response = await axios.get(url);
-    const end = Date.now();
-    const data = response.data;
-    console.log(`| ${api} | ${end - start}ms | ${JSON.stringify(data, null, 2)} |`);
+    await axios.get(url).then((response) => {
+      const end = Date.now();
+      const data = response.data;
+      console.log(data);
+      return { "time": (end - start), "data": JSON.stringify(data) };
+    });
+
   } catch (error) {
-    console.error(`| ${api} | Error | ${error.message} |`);
+    console.error(error);
   }
 }
 
-console.log('| API | Response Time | Output |');
-console.log('| --- | --- | --- |');
 
-for (const [api, details] of Object.entries(sites)) {
-  const url = details.server || details.client;
-  testAPI(api, url);
-}
+(async () => {
+  for (const [api, details] of Object.entries(sites)) {
+    if (details.client && details.client.length > 0) {
+      if (details.client.includes('YOUR-APIKEY')) {
+        continue;
+      }
+
+      testAPI(details.client).then((result) => {
+        console.log(result)
+        sites.api["clientTime"] = result.time;
+        sites.api["clientData"] = result.data;
+      });
+    }
+
+    if (details.server && details.server.length > 0) {
+      if (details.server.includes('YOUR-APIKEY')) {
+        continue;
+      }
+
+      testAPI(details.server).then((result) => {
+        sites.api["serverTime"] = result.time;
+        sites.api["serverData"] = result.data;
+      });
+    }
+  }
+})();
+
+fs.writeFileSync('output.json', JSON.stringify(sites, null, 2));
