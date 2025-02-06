702ced9bcc43f9b7bb7c7419f0f2d510a83be7d7
C4illin <emrik.ostling@gmail.com>
2025-02-06 18:42:00 +0100

    rerun and format

--- header.md
+++ header.md
@@ -1,4 +1,5 @@
 # GeoIP-api comparison
+
 Comparison of some different mostly free GeoIP APIs I found around the internet.
 
 Go to https://geoip.emrik.org to do your own benchmark or lookup an IP address using multiple APIs.
--- proxy.js
+++ proxy.js
@@ -9,7 +9,7 @@ const maxmindaccount = process.env.maxmindaccount;
 console.log("Using maxmind account id: " + maxmindaccount);
 const geoipClient = new WebServiceClient(maxmindaccount, geoipKey);
 const geoliteClient = new WebServiceClient(maxmindaccount, geoipKey, {
-	host: "geolite.info",
+  host: "geolite.info",
 });
 
 const app = express();
@@ -17,48 +17,48 @@ const port = 3000;
 app.use(cors());
 
 app.get("/:site/:ip", (req, res) => {
-	const site = req.params.site;
-	const ip = req.params.ip;
+  const site = req.params.site;
+  const ip = req.params.ip;
 
-	if (site == "geoip2") {
-		geoipClient
-			.city(ip)
-			.then((response) => {
-				res.send(response);
-			})
-			.catch((error) => {
-				res.send("Error with request to " + site);
-			});
-	} else if (site == "geolite2") {
-		geoliteClient
-			.city(ip)
-			.then((response) => {
-				res.send(response);
-			})
-			.catch((error) => {
-				res.send("Error with request to " + site);
-			});
-	} else if (site in sites) {
-		let url = sites[site]["server"].replace("8.8.8.8", ip);
+  if (site == "geoip2") {
+    geoipClient
+      .city(ip)
+      .then((response) => {
+        res.send(response);
+      })
+      .catch((error) => {
+        res.send("Error with request to " + site);
+      });
+  } else if (site == "geolite2") {
+    geoliteClient
+      .city(ip)
+      .then((response) => {
+        res.send(response);
+      })
+      .catch((error) => {
+        res.send("Error with request to " + site);
+      });
+  } else if (site in sites) {
+    let url = sites[site]["server"].replace("8.8.8.8", ip);
 
-		// fetch url and return response
-		axios
-			.get(url)
-			.then((response) => {
-				res.send(response.data);
-			})
-			.catch((error) => {
-				res.send("Error with request to " + site);
-			});
-	} else {
-		res.send("Site not found");
-	}
+    // fetch url and return response
+    axios
+      .get(url)
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((error) => {
+        res.send("Error with request to " + site);
+      });
+  } else {
+    res.send("Site not found");
+  }
 });
 
 app.get("/", (req, res) => {
-	res.send("Site not found");
+  res.send("Site not found");
 });
 
 app.listen(port, () => {
-	console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
